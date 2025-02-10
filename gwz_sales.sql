a04983929caded5354ecb972d3607ba25c2dc31c
fadodo <fadodo@users.noreply.github.com>
2025-02-10 12:28:18 +0100

    sales_sortes_by_date

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,6 @@
 SELECT date_date
-, ROUND(SUM(turnover),2) As turnover
-, ROUND(SUM(purchase_cost),2) AS purchase_cost
+, ROUND(SUM(turnover),2) As turnover_sorted
+, ROUND(SUM(purchase_cost),2) AS purchase_cost_sorted
 FROM `data-analytics-bootcamp-363212.bi_dataset.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date;
